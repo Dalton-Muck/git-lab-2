556b6eefe2c7875ddb2b6dcc780f61b1f888c262
Dalton-Muck <dalton.muck22@icloud.com>
2023-01-29 19:56:38 -0500

    Initial commit

---
+++ git-lab-program.cc/git-lab-program.cc
@@ -0,0 +1,17 @@
+/**
+* @file: git-lab-program.cc
+* @author: Dalton Muck
+* @date: 01-29-2023
+* @breif: Test program for the Git introduction lab
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    cout << "Hello git!" << endl;
+   return 0;
+} //main
